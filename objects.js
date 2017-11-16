18832392a50044ece823df93f9ac95dab889d085
April Rivera <aprilcr87@gmail.com>
2017-11-16 22:53:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,3 +10,9 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi');
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  delete playlist.artistName;
+  return playlist;
+}
+removeFromPlaylist('Slowdive');
